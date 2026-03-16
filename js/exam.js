022db3ba04e39d2a489f406d2f73e460ceb91fed
irsanwu01 <irsan@jabotabekns.co.id>
2026-03-16 10:46:38 +0700

    Update exam.js

--- js/exam.js
+++ js/exam.js
@@ -2,14 +2,17 @@ let questions = []
 let current = 0
 let answers = []
 
+const label = ["A","B","C","D"]
+
 let params = new URLSearchParams(location.search)
 let set = params.get("set") || 1
 
 fetch("questions/set"+set+".json")
-.then(r => r.json())
-.then(data => {
+.then(r=>r.json())
+.then(data=>{
 
 questions = data
+
 load()
 
 })
@@ -21,12 +24,14 @@ if(!questions.length) return
 let q = questions[current]
 
 document.getElementById("title").innerText =
-"Question " + (current+1)
+"Question " + (current+1) + " / " + questions.length
 
 let html = q.hanzi
 
 if(q.image){
-html += "<br><img src='"+q.image+"' style='max-width:400px'>"
+
+html += "<br><img src='"+q.image+"' style='max-width:420px;margin-top:10px'>"
+
 }
 
 document.getElementById("questionBox").innerHTML = html
@@ -35,7 +40,13 @@ let opt=""
 
 q.options.forEach((o,i)=>{
 
-opt += `<button onclick="select(${i})">${o}</button><br>`
+let selected = ""
+
+if(answers[current] == i){
+selected = "style='background:#ffe082'"
+}
+
+opt += `<button ${selected} onclick="select(${i})">${label[i]}. ${o}</button><br>`
 
 })
 
@@ -44,14 +55,31 @@ document.getElementById("options").innerHTML = opt
 }
 
 function select(i){
+
 answers[current] = i
+
+if(current < questions.length-1){
+
+current++
+
+load()
+
+}else{
+
+load()
+
+}
+
 }
 
 function next(){
 
 if(current < questions.length-1){
+
 current++
+
 load()
+
 }
 
 }
@@ -59,8 +87,11 @@ load()
 function prev(){
 
 if(current > 0){
+
 current--
+
 load()
+
 }
 
 }
@@ -70,9 +101,19 @@ function submitExam(){
 let correct = 0
 
 questions.forEach((q,i)=>{
-if(answers[i]==q.answer) correct++
+
+if(answers[i] == q.answer){
+correct++
+}
+
 })
 
-alert("Score: "+correct+" / "+questions.length)
+let score = Math.round((correct / questions.length) * 100)
+
+alert(
+"Correct : " + correct +
+"\nTotal : " + questions.length +
+"\nScore : " + score + "%"
+)
 
 }
