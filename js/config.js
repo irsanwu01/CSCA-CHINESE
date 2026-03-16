ec7bbc0a51d18cb314fb224fc2d09f652e4f809b
irsanwu01 <irsan@jabotabekns.co.id>
2026-03-16 22:34:07 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,20 +1,12 @@
-/* =========================
-   SUPABASE CONFIG
-========================= */
-
 const SUPABASE_URL = "https://rgswtegsanbwtajqhmgy.supabase.co"
 
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnc3d0ZWdzYW5id3RhanFobWd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2MjQxMzIsImV4cCI6MjA4OTIwMDEzMn0.OW8IA3_AQy8gFkzjB8n8YlzyoXmvkeoSRelGHybAI7Q"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
 
-
-
-/* =========================
-   INIT SUPABASE
-========================= */
+window.db = null
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    if (typeof supabase === "undefined") {
+    if(typeof supabase === "undefined"){
         console.error("Supabase library not loaded")
         return
     }
@@ -24,7 +16,6 @@ document.addEventListener("DOMContentLoaded", () => {
         SUPABASE_KEY
     )
 
-    console.log("Supabase connected")
-    console.log(window.db)
+    console.log("Supabase ready")
 
 })
