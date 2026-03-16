b13bf241367e39d8d6ff6825e0031a374906b635
irsanwu01 <irsan@jabotabekns.co.id>
2026-03-16 16:16:40 +0700

    Update exam.js

--- js/exam.js
+++ js/exam.js
@@ -137,7 +137,9 @@ alert("Score: "+score+" / "+questions.length)
 function drawGraph(text){
 
 let canvas=document.getElementById("graph")
-if(!canvas) return
+
+canvas.width=canvas.parentElement.offsetWidth
+canvas.height=260
 
 if(chart){
 chart.destroy()
@@ -146,13 +148,54 @@ chart=null
 
 text=text.replace(/\*/g,"")
 
+
+
+/* ===== DETECT TWO POINTS ===== */
+
+let coords=[...text.matchAll(/\((-?\d+)\s*,\s*(-?\d+)\)/g)]
+
+if(coords.length>=2){
+
+let x1=parseFloat(coords[0][1])
+let y1=parseFloat(coords[0][2])
+
+let x2=parseFloat(coords[1][1])
+let y2=parseFloat(coords[1][2])
+
+chart=new Chart(canvas,{
+type:'scatter',
+data:{
+datasets:[{
+data:[
+{x:x1,y:y1},
+{x:x2,y:y2}
+],
+showLine:true,
+borderColor:"blue",
+pointRadius:6
+}]
+},
+options:{
+plugins:{legend:{display:false}},
+responsive:true,
+maintainAspectRatio:false
+}
+})
+
+return
+}
+
+
+
+/* ===== DETECT HYPERBOLA ===== */
+
 let hyper=text.match(/x.?2\/(\d+)\s*-\s*y.?2\/(\d+)/i)
+
 if(!hyper) return
 
 let a=Math.sqrt(parseFloat(hyper[1]))
 let b=Math.sqrt(parseFloat(hyper[2]))
 let c=Math.sqrt(a*a+b*b)
-
 
 let rightTop=[]
 let rightBottom=[]
@@ -170,7 +213,6 @@ leftTop.push({x:-x,y:y})
 leftBottom.push({x:-x,y:-y})
 
 }
-
 
 chart=new Chart(canvas,{
 type:'scatter',
@@ -220,10 +262,8 @@ pointRadius:6
 
 options:{
 plugins:{legend:{display:false}},
-
 responsive:true,
 maintainAspectRatio:false,
-
 scales:{
 x:{min:-6,max:6,ticks:{stepSize:1}},
 y:{min:-6,max:6,ticks:{stepSize:1}}
@@ -233,8 +273,6 @@ y:{min:-6,max:6,ticks:{stepSize:1}}
 })
 
 
-
-/* LABEL FOKUS */
 
 setTimeout(function(){
 
