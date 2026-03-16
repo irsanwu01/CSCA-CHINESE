9ca8c38d733a365105a49a9f0c2087c2a3f06608
irsanwu01 <irsan@jabotabekns.co.id>
2026-03-16 16:55:44 +0700

    Update exam.js

--- js/exam.js
+++ js/exam.js
@@ -7,7 +7,7 @@ let chart=null
 
 async function loadSet(){
 
-let res=await fetch("./question/set1.json")
+let res=await fetch("./question/set"+window.currentSet+".json")
 questions=await res.json()
 
 showQuestion()
