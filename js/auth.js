035f1b0a9125ea6a829f0ea8e88aec91947f325a
irsanwu01 <irsan@jabotabekns.co.id>
2026-03-16 16:48:21 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,50 @@
-function login(){
-alert("Login system coming soon")
+async function register(){
+
+let email=document.getElementById("email").value
+let password=document.getElementById("password").value
+
+let {data,error}=await window.db.auth.signUp({
+email:email,
+password:password
+})
+
+if(error){
+alert(error.message)
+return
 }
 
+alert("Register success")
+window.location="login.html"
+
+}
+
+
+
+async function login(){
+
+let email=document.getElementById("email").value
+let password=document.getElementById("password").value
+
+let {data,error}=await window.db.auth.signInWithPassword({
+email:email,
+password:password
+})
+
+if(error){
+alert(error.message)
+return
+}
+
+window.location="exam.html"
+
+}
+
+
+
+async function logout(){
+
+await window.db.auth.signOut()
+
+window.location="login.html"
+
+}
