e7f1edf08bf68121e7b8f8c5317335a84a3563c2
irsanwu01 <irsan@jabotabekns.co.id>
2026-03-16 22:35:13 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,20 +2,12 @@ const SUPABASE_URL = "https://rgswtegsanbwtajqhmgy.supabase.co"
 
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
 
-window.db = null
+const { createClient } = window.supabase
 
-document.addEventListener("DOMContentLoaded", () => {
+window.db = createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+)
 
-    if(typeof supabase === "undefined"){
-        console.error("Supabase library not loaded")
-        return
-    }
-
-    window.db = supabase.createClient(
-        SUPABASE_URL,
-        SUPABASE_KEY
-    )
-
-    console.log("Supabase ready")
-
-})
+console.log("Supabase connected")
+console.log(window.db)
