a1ef8e9dd4be2b3819037c4c5b85a2be94fea14b
irsanwu01 <irsan@jabotabekns.co.id>
2026-03-16 10:33:23 +0700

    Update exam.js

--- js/exam.js
+++ js/exam.js
@@ -1,122 +1,112 @@
-let questions=[]
-let current=0
-let answers=[]
+let questions = []
+let current = 0
+let answers = []
 
-let params=new URLSearchParams(location.search)
-let set=params.get("set") || 1
+let params = new URLSearchParams(location.search)
+let set = params.get("set") || 1
 
+// load question set
 fetch("questions/set"+set+".json")
-.then(r=>r.json())
-.then(data=>{
-questions=data
-load()
+.then(r => r.json())
+.then(data => {
+    questions = data
+    load()
 })
 
+// load question
 function load(){
 
-let q=questions[current]
+    let q = questions[current]
 
-document.getElementById("title").innerText="Question "+(current+1)
+    document.getElementById("title").innerText =
+    "Question " + (current + 1)
 
-document.getElementById("questionBox").innerText=q.hanzi
+    // soal + gambar
+    let questionHTML = q.hanzi
 
-let html=""
+    if(q.image){
+        questionHTML +=
+        "<br><img src='"+q.image+"' style='max-width:100%;height:auto;margin-top:10px'>"
+    }
 
-q.options.forEach((o,i)=>{
-html+=`<button onclick="select(${i})">${o}</button><br>`
-})
+    document.getElementById("questionBox").innerHTML = questionHTML
 
-document.getElementById("options").innerHTML=html
+    // pilihan jawaban
+    let optionsHTML = ""
+
+    q.options.forEach((o,i)=>{
+
+        let checked = ""
+
+        if(answers[current] == i){
+            checked = "style='background:#ffe082'"
+        }
+
+        optionsHTML +=
+        `<button ${checked} onclick="select(${i})">${o}</button><br>`
+
+    })
+
+    document.getElementById("options").innerHTML = optionsHTML
 
 }
 
+// pilih jawaban
 function select(i){
-answers[current]=i
+
+    answers[current] = i
+
+    load()
+
 }
 
+// next question
 function next(){
-if(current<questions.length-1){
-current++
-load()
-}
+
+    if(current < questions.length - 1){
+
+        current++
+
+        load()
+
+    }
+
 }
 
+// previous question
 function prev(){
-if(current>0){
-current--
-load()
-}
+
+    if(current > 0){
+
+        current--
+
+        load()
+
+    }
+
 }
 
+// submit exam
 function submitExam(){
 
-let correct=0
+    let correct = 0
 
-questions.forEach((q,i)=>{
-if(answers[i]==q.answer) correct++
-})
+    questions.forEach((q,i)=>{
 
-alert("Score: "+correct)
+        if(answers[i] == q.answer){
 
-}
-let questions=[]
-let current=0
-let answers=[]
+            correct++
 
-let params=new URLSearchParams(location.search)
-let set=params.get("set") || 1
+        }
 
-fetch("questions/set"+set+".json")
-.then(r=>r.json())
-.then(data=>{
-questions=data
-load()
-})
+    })
 
-function load(){
+    let score = Math.round((correct / questions.length) * 100)
 
-let q=questions[current]
-
-document.getElementById("title").innerText="Question "+(current+1)
-
-document.getElementById("questionBox").innerText=q.hanzi
-
-let html=""
-
-q.options.forEach((o,i)=>{
-html+=`<button onclick="select(${i})">${o}</button><br>`
-})
-
-document.getElementById("options").innerHTML=html
-
-}
-
-function select(i){
-answers[current]=i
-}
-
-function next(){
-if(current<questions.length-1){
-current++
-load()
-}
-}
-
-function prev(){
-if(current>0){
-current--
-load()
-}
-}
-
-function submitExam(){
-
-let correct=0
-
-questions.forEach((q,i)=>{
-if(answers[i]==q.answer) correct++
-})
-
-alert("Score: "+correct)
+    alert(
+    "Correct : " + correct +
+    "\nTotal : " + questions.length +
+    "\nScore : " + score
+    )
 
 }
